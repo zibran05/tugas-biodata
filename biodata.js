e5c6999e2650c73b8f6954d09bce4ead0f677488
zibran05 <zibranmalisi34@gmail.com>
2024-09-23 23:18:35 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,6 @@
+const Nama = document.querySelector('#Nama');
+Nama.textContent = 'ZIBRAN';
+const TTL = document.querySelector('#TTL');
+TTL.textContent = 'sukabumi,7april2004';
+const Alamat = document.querySelector('#Alamat');
+Alamat.textContent = 'Sukabumi, jl.rancakadu';
